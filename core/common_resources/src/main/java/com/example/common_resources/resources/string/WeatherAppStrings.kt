498959743bed0652c37dc42ce8c214c96d709b6e
Nauruz-Guliev <nauruz9248327@gmail.com>
2023-01-29 13:43:32 +0300

    + files removal

--- core/common_resources/src/main/java/com/example/common_resources/resources/string/WeatherAppStrings.kt
+++
@@ -1,9 +0,0 @@
-package com.example.common_resources.resources.string
-
-import com.example.common_resources.R
-
-object WeatherAppStrings {
-    val error = R.string.error
-    val locationError = R.string.location_error
-    val locationUnknownError = R.string.location_error_unknown
-}
