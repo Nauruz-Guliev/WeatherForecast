52e1a6fafcfc41f8455f4bd11ff1d4231d0e022a
Nauruz-Guliev <nauruz9248327@gmail.com>
2023-01-29 13:42:03 +0300

    + common resources

---
+++ core/common_resources/src/main/java/com/example/common_resources/resources/icon/WeatherAppIcons.kt
@@ -0,0 +1,10 @@
+package com.example.common_resources.resources.icon
+
+import com.example.common_resources.R
+
+object WeatherAppIcons {
+    val Search = R.drawable.baseline_search_24
+    val Settings = R.drawable.baseline_settings_24
+    val Map = R.drawable.baseline_map_24
+    val Home = R.drawable.baseline_home_24
+}
